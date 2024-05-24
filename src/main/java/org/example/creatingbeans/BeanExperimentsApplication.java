e0c6c2dc5fd96eb6e6f8fa63889a85f61052b778
Marcos Martins <marcos_36ma@hotmail.com>
2024-05-23 21:10:55 -0300

    first commit

---
+++ src/main/java/org/example/creatingbeans/BeanExperimentsApplication.java
@@ -0,0 +1,7 @@
+package org.example.creatingbeans;
+
+public class BeanExperimentsApplication {
+    public static void main(String[] args) {
+        RandomService randomService = new RandomService();
+    }
+}
---
+++ src/main/java/org/example/creatingbeans/RandomService.java
@@ -0,0 +1,5 @@
+package org.example.creatingbeans;
+
+public class RandomService {
+
+}
